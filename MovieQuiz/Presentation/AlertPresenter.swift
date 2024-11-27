f4723633d876f35f300bf3e5ce5271b45f81e117
Yura <gije.ger@gmail.com>
2024-11-27 20:23:08 +0400

    Добавлен новый функционал в игру, с сохранением лучшего результата

---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,34 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by Yura on 19.11.24.
+//
+
+import UIKit
+import Foundation
+
+final class AlertPresenter {
+    
+    private weak var viewController: UIViewController?
+    
+    init(viewController: UIViewController? = nil) {
+        self.viewController = viewController
+    }
+    
+    func showAlert(model: AlertModel) {
+        let alert = UIAlertController(
+            title: model.title,
+            message: model.message,
+            preferredStyle: .alert
+        )
+        
+        let action = UIAlertAction(title: model.buttonText, style: .default) { _ in
+            model.completion?()
+        }
+        
+        alert.addAction(action)
+
+        viewController?.present(alert, animated: true)
+    }
+}
